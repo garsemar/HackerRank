42da1136b6aef949baad03e0fa4735d070aee8af
garsemar <martigarciaserra@gmail.com>
2022-01-31 19:33:44 +0100

    diagonalDifference

--- diagonal-difference.py
+++ diagonal-difference.py
@@ -16,10 +16,26 @@ import sys
 
 def diagonalDifference():
     # Write your code here
-    matrix = [[1, 2, 3],
-              [4, 5, 6],
-              [7, 8, 9]]
+    matrix = []
+
+    num = int(input())
+    for i in range(num):
+        lis = [int(numlist) for numlist in input().split(" ")]
+        matrix.append(lis)
+    bb = 0
+    for i in range(len(matrix)):
+        bb += matrix[i][i]
+        pass
+    a = 0
+    b = len(matrix[0]) - 1
+    aa = 0
+    for j in range(len(matrix)):
+        aa += matrix[a][b]
+        a += 1
+        b -= 1
+
+    return abs(aa-bb)
 
 
 if __name__ == '__main__':
-    diagonalDifference()
+    print(diagonalDifference())
