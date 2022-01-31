64782c0a47bf81d6ec991453d47b5ad5c0012420
garsemar <martigarciaserra@gmail.com>
2022-01-31 13:58:55 +0100

    diagonalDifference

---
+++ diagonal-difference.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference():
+    # Write your code here
+    matrix = [[1, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9]]
+
+
+if __name__ == '__main__':
+    diagonalDifference()
